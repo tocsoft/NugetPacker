41a89faac467469d83cae640eb491aa124cab511
Scott Williams <scott.williams@2020legal.co.uk>
2016-08-23 15:23:28 +0100

    fix versioning logic

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo Off
 ECHO Starting build
 
-set version=-Version 0.0.0
+set version=
 
 if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
@@ -10,6 +10,10 @@ if not "%GitVersion_NuGetVersion%" == "" (
 	if "%version%" == "" (
 		set version=-Version %GitVersion_NuGetVersion%
 	)
+)
+
+if "%version%" == "" (
+	set version=-Version 0.0.0
 )
 
 ECHO Building pacakge
